933f6f3dcb1f839a8628c7f92ad9cbde6d2032f3
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-05-24 12:02:14 +0300

    added Component class

--- app/src/main/java/com/kirillmesh/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/kirillmesh/dependencyinjectionstart/example1/Activity.kt
@@ -1,13 +1,12 @@
 package com.kirillmesh.dependencyinjectionstart.example1
 
 class Activity {
-    val monitor = Monitor()
-    val keyboard = Keyboard()
-    val mouse = Mouse()
-    val systemBlock = SystemBlock(
-        Memory(),
-        Processor(),
-        Storage()
-    )
-    val computer = Computer(monitor, systemBlock, keyboard, mouse)
+
+    //val computer = Component.getComputer()
+    lateinit var computer: Computer
+    lateinit var keyboard: Keyboard
+
+    init {
+        Component().inject(this)
+    }
 }
---
+++ app/src/main/java/com/kirillmesh/dependencyinjectionstart/example1/Component.kt
@@ -0,0 +1,21 @@
+package com.kirillmesh.dependencyinjectionstart.example1
+
+class Component {
+
+    private fun getComputer(): Computer {
+        val monitor = Monitor()
+        val keyboard = Keyboard()
+        val mouse = Mouse()
+        val systemBlock = SystemBlock(
+            Memory(),
+            Processor(),
+            Storage()
+        )
+        return Computer(monitor, systemBlock, keyboard, mouse)
+    }
+
+    fun inject(activity: Activity){
+        activity.computer = getComputer()
+        activity.keyboard = Keyboard()
+    }
+}
