3b3912fa2fa047e6a0f179907ebaeb42fd89e01b
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-05-25 16:29:28 +0300

    dagger 2 injection example2 (View model Factory, @IntoMap, @StringKey)

--- app/src/main/java/com/kirillmesh/dependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
+++ app/src/main/java/com/kirillmesh/dependencyinjectionstart/example2/presentation/ExampleViewModel2.kt
@@ -2,15 +2,15 @@ package com.kirillmesh.dependencyinjectionstart.example2.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.kirillmesh.dependencyinjectionstart.example2.domain.ExampleUseCase
+import com.kirillmesh.dependencyinjectionstart.example2.domain.ExampleRepository
 import javax.inject.Inject
 
-class ExampleViewModel @Inject constructor(
-    private val useCase: ExampleUseCase
+class ExampleViewModel2 @Inject constructor(
+    private val repository: ExampleRepository
 ) : ViewModel() {
 
     fun method() {
-        useCase()
+        repository.method()
         Log.d("ExampleViewModel", "$this")
     }
 }
--- app/src/main/java/com/kirillmesh/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/kirillmesh/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kirillmesh.dependencyinjectionstart.example2.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.kirillmesh.dependencyinjectionstart.R
 import com.kirillmesh.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
@@ -9,7 +10,15 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
+
+    private val viewModel2 by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel2::class.java]
+    }
 
     private val component by lazy {
         (application as ExampleApp).component
@@ -21,5 +30,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewModel.method()
+        viewModel2.method()
     }
 }
